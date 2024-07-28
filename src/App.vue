745a27ff49262e4cee49b023f3c23db5f9d7e4b9
nOo9ya <noo9ya@gmail.com>
2024-07-28 22:53:51 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,111 @@
+<script lang="ts">
+import HelloWorld from "@/components/HelloWorld.vue";
+import TheWelcome from "@/components/TheWelcome.vue";
+import JobsList from '@/components/JobsList.vue';
+import { defineComponent, reactive, ref, toRefs } from "vue";
+import { Job } from "./types/job";
+
+export default defineComponent({
+  name: "App",
+  components: {JobsList, TheWelcome, HelloWorld },
+  setup() {
+    // ex 1 : use reactive
+    // const state = reactive({
+    //   name: 'Link',
+    //   age: 20 as string | number
+    // });
+    // state.name = 'Link2';
+    // state.age = 24;
+    // return { ...toRefs(state) };
+
+    // ex 2
+    // const name = ref('link');
+    // // const age = ref(20) as number | string; <- error
+    // const age = ref<number|string>(25); // <- correct
+    // return { name, age };
+
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarry man",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      { title: "fisher man", location: "lake hylia", salary: 21000, id: "4" },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
+
+    return { jobs };
+  },
+  methods: {
+    //
+  },
+});
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <JobsList :jobs="jobs" />
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/JobsList.vue
@@ -0,0 +1,81 @@
+<script lang="ts">
+// defineProps<{
+//   jobs: Job[]
+// }>()
+//
+// defineComponent({
+//   methods: {
+//     //
+//   }
+// })
+
+import {defineComponent, PropType} from 'vue';
+import type {Job} from "@/types/job";
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>
+    }
+  }
+});
+</script>
+
+<template>
+  <div class="job-list">
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>{{ job.salary }} won</p>
+        </div>
+        <div class="description">
+          <p>{{ job.location }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.job-list {
+  max-width: 960px;
+  margin: 40px auto;
+}
+
+.job-list ul {
+  padding: 0
+}
+
+.job-list li {
+  list-style-type: none;
+  background: white;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+}
+
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+
+.salary {
+  display: flex;
+}
+
+.salary img {
+  width: 30px;
+}
+
+.salary p {
+  color: #17bf66;
+  font-weight: bold;
+  margin: 10px 4px;
+}
+
+.list-move {
+  transition: all 1s;
+}
+</style>
